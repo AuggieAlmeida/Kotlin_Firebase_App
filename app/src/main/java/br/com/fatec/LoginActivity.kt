bdad2683a3ec1c280b94d59a6500f7191adce107
Gael <augustob.almeida12@gmail.com>
2023-03-30 01:53:16 -0300

    SingIn build

--- app/src/main/java/br/com/fatec/LoginActivity.kt
+++ app/src/main/java/br/com/fatec/LoginActivity.kt
@@ -5,18 +5,63 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.awt.Button
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = Firebase.auth
+
+
         val registertext: TextView = findViewById(R.id.txt_cadastrar)
+        var loginButton : Button = findViewById(R.id.btn_login)
 
         registertext.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        loginButton.setOnClickListener {
+            val email = findViewById<EditText>(R.id.email_log)
+            val password = findViewById<EditText>(R.id.password_log)
+
+            val inputEmail = email.text.toString()
+            val inputPassword = password.text.toString()
+
+            performSignIn(inputEmail, inputPassword)
+        }
     }
+
+    private fun performSignIn(email: String, password: String) {
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val intent = Intent(
+                        this,
+                        HomeActivity::class.java
+                    )
+
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
 }
